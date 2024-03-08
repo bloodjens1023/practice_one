955bbca4bcd5a545713a0ced41b07b16fd826a0a
bloodjens1023 <bloodjens32@gmail.com>
2024-03-08 22:29:25 +0100

    buy btn

---
+++ front/src/components/BuyBtn.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+
+const BuyBtn = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = "https://js.stripe.com/v3/buy-button.js";
+    script.async = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return (
+    <stripe-buy-button
+      buy-button-id="buy_btn_1OsAvYFDP4bEYovMPIOPjZfX"
+      publishable-key="pk_test_51OsAfMFDP4bEYovMxgekldjZEbHkuvuZRq2HfCLbG2KnvjIXioJ9xmDdIVA1Ebg6MVN863hgNl1FMGhCkDi6HAj10051QD7oLX"
+    />
+  );
+};
+
+export default BuyBtn;
